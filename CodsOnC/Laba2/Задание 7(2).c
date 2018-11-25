6e6d258a43142fe85507822a81499979c7d2a83c
Eugene <44334164+LakadMatataaaag@users.noreply.github.com>
2018-11-25 19:55:17 +0600

    Add files via upload

--- CodsOnC/Laba2/Задание 7(2).c
+++ CodsOnC/Laba2/Задание 7(2).c
@@ -1,23 +1,27 @@
-/* Используя Булевый инклуд и сдвиг по байтам из-за деления на 2.
-   Ваще топово конечно, но фак, через рекурсию надо было, Алексей Дмитриевич не бейте >_<
-*/
+float Newton(float x, float current, float next)
+{
+    if (current - next > 0.00001)
+    {
+        current = next;
+        next = (current + x/current)/2.0f;
+        return Newton(x, current, next);
+    }
+    else
+    {
+        return next;
+    }
+}
+
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int x = 1;
-    bool decreased = false;
-    for (;;)
-    {
-    	int nx = (x + n / x) >> 1;
-    	if (x == nx || nx > x && decreased)  break;
-    	    decreased = nx < x;
-    	x = nx;
-    }
-    printf("%d\n", x);
-
+    float x;
+    printf("Введите число\n");
+    scanf("%f", &x);
+    float y = x / 2.0f;
+    float A1 = (y + x/y) / 2.0f;
+    float sqr = Newton(x, y, A1);
+    printf("Корень из %.0f = %.5f\n", x, sqr);
     return 0;
 }
