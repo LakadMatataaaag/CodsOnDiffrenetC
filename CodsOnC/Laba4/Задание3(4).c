3ae7ce082e93887a72935c54d026b6752465dd1f
Eugene <44334164+LakadMatataaaag@users.noreply.github.com>
2018-11-29 08:57:27 +0600

    Update Задание3(4).c

--- CodsOnC/Laba4/Задание3(4).c
+++ CodsOnC/Laba4/Задание3(4).c
@@ -1,22 +1,22 @@
-unsigned int str_length(const char* s)
-{
-    int i = 0;
-    do
-    {
-        i++;
-    } while(s[i]);
-    return i;
-}
+#include <stdio.h> 
+
+unsigned int str_length(const char* s) 
+{ 
+    int i = 0; 
+    while (*s != '\0') 
+    { 
+        i++; 
+        s++; 
+    } 
+    return i; 
+} 
 
 
+int main() 
+{ 
+    char arr[100]; 
+    scanf("%s", &arr); 
+    printf("%d\n", str_length(arr)); 
 
-#include <stdio.h>
-
-int main()
-{
-    char arr[100];
-    scanf("%s", &arr);
-    printf("%d\n", str_length(arr));
-
-    return 0;
+    return 0; 
 }
