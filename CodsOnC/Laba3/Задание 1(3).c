3fdf2ed64cfc30647aca9ede4749dbbe6d2ae1de
Eugene <44334164+LakadMatataaaag@users.noreply.github.com>
2018-12-10 18:34:14 +0600

    Add files via upload

--- CodsOnC/Laba3/Задание 1(3).c
+++ CodsOnC/Laba3/Задание 1(3).c
@@ -1,27 +1,40 @@
 #include <stdio.h>
-
-int func (char x[100], char y[100])
+#define N 5
+#define D 2
+int fun (char a[], char b[])
 {
-    int count = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        if (x[i] != y[i])
-        {
-            count++;
-        }
+    int i;
+    int c = 0;
 
+    for (i = 0; i < N; i++)
+    {
+        if (a[i] != b[i])
+        {
+            c++;
+        }
     }
-    return count;
+
+    return c;
 }
 
 int main()
 {
-    char a[100];
-    char b[100];
-    scanf("%s", a);
-    scanf("%s", b);
-    int c = 0;
-    c = func(a , b);
-    printf("%d\n", c);
+    char* arr[] = {"StrStr", "Testtr"};
+    int m = 0;
+    int i;
+    int j;
+    int c;
+    for (i = 0; i < D; i++)
+    {
+        for (j = 0; j < D; j++)
+        {
+            c = fun(arr[i],arr[j]);
+            if (c > m)
+            {
+                m = c;
+            }
+        }
+    }
+    printf("%d\n", m);
     return 0;
 }
